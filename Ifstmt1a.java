56401d1d9d86117b14e378d36947d751a3cf1a38
Ravella Susmitha <23bq1a12a3@vvit.net>
2026-07-04 18:06:41 +0530

    Add files via upload

---
+++ Ifstmt1a.java
@@ -0,0 +1,22 @@
+
+
+
+package Controlstatements;
+
+public class Ifstmt1a {
+
+    public static void main(String[] args) {
+
+        int num = Integer.parseInt(args[0]);
+
+        if(num > 0) {
+            System.out.println("Positive");
+        }
+        else if(num < 0) {
+            System.out.println("Negative");
+        }
+        else {
+            System.out.println("Zero");
+        }
+    }
+}
---
+++ Ifstmt2.java
@@ -0,0 +1,17 @@
+package Controlstatements;
+
+public class Ifstmt2 {
+
+
+	    public static void main(String[] args) {
+
+	        int num = Integer.parseInt(args[0]);
+
+	        if(num % 2 == 0) {
+	            System.out.println("Even");
+	        }
+	        else {
+	            System.out.println("Odd");
+	        }
+	    }
+	}
