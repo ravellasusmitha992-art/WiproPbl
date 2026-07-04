61427bf77a90f27fc61d23f36c81770d80c7ecb0
Ravella Susmitha <23bq1a12a3@vvit.net>
2026-07-04 18:04:36 +0530

    Add files via upload

---
+++ Commandlinearg2.java
@@ -0,0 +1,21 @@
+package Languagebasics;
+
+public class Commandlinearg2 {
+	public static void main(String[] args) {
+
+
+	        if(args.length >= 2) {
+
+	            int a = Integer.parseInt(args[0]);
+	            int b = Integer.parseInt(args[1]);
+
+	            int sum = a + b;
+
+	            System.out.println("The sum of " + a + " and " + b + " is " + sum);
+
+	        } else {
+	            System.out.println("Please enter two numbers");
+	        }
+	    }
+}
+	
