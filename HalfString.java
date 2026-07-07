2b3cfbabcd471662ed4f4bcb870e7d2732622e99
Ravella Susmitha <23bq1a12a3@vvit.net>
2026-07-07 09:37:10 +0530

    Add files via upload

---
+++ HalfString.java
@@ -0,0 +1,14 @@
+package StringStringbuffer;
+
+public class HalfString {
+    public static void main(String[] args) {
+
+        String str = "TomCat";
+
+        if (str.length() % 2 == 0) {
+            System.out.println(str.substring(0, str.length() / 2));
+        } else {
+            System.out.println("null");
+        }
+    }
+}
