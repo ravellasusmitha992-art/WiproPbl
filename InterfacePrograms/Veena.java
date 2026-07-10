43aedac558e20fcde038ee367696e74b0f52b8fc
Ravella Susmitha <23bq1a12a3@vvit.net>
2026-07-10 09:52:27 +0530

    Add files via upload

---
+++ InterfacePrograms/Veena.java
@@ -0,0 +1,10 @@
+package Interfaces.music.string;
+
+import Interfaces.music.Playable;
+
+public class Veena implements Playable {
+
+    public void play() {
+        System.out.println("Playing Veena");
+    }
+}
