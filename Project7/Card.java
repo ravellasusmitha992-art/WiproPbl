eba52284bf27412d58e5400eeb604a6ad6d6a75a
Ravella Susmitha <23bq1a12a3@vvit.net>
2026-07-15 16:27:51 +0530

    Add files via upload

---
+++ Project7/Card.java
@@ -0,0 +1,21 @@
+package Project7;
+
+
+public class Card {
+
+    private String symbol;
+    private int number;
+
+    public Card(String symbol, int number) {
+        this.symbol = symbol;
+        this.number = number;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
