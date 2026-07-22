8b279ab5f296af4a8a9a922d88462d6c65fad0da
Ravella Susmitha <23bq1a12a3@vvit.net>
2026-07-22 11:59:21 +0530

    Add files via upload

---
+++ MultiThreadingMiniProject/HareTortoiseRaceB.java
@@ -0,0 +1,48 @@
+package threadcreation;
+
+public class HareTortoiseRaceB {
+
+    static volatile boolean raceFinished = false;
+
+    static class Race extends Thread {
+
+        String animal;
+
+        Race(String animal) {
+            this.animal = animal;
+        }
+
+        public void run() {
+
+            for (int i = 1; i <= 100; i++) {
+
+                if (raceFinished) {
+                    return;
+                }
+
+                System.out.println(animal + " ran " + i + " meters");
+
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            raceFinished = true;
+            System.out.println(animal + " wins the race!");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Race hare = new Race("Hare");
+        Race tortoise = new Race("Tortoise");
+
+        hare.setPriority(Thread.MAX_PRIORITY);
+        tortoise.setPriority(Thread.MIN_PRIORITY);
+
+        hare.start();
+        tortoise.start();
+    }
+}
