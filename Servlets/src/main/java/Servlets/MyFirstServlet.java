917c9a71d079269d407b900800a7a57d8294e50f
Ravella Susmitha <ravellasusmitha992@gmail.com>
2026-08-09 18:19:48 +0530

    Completed ServletsAssignment1

---
+++ Servlets/src/main/java/Servlets/MyFirstServlet.java
@@ -0,0 +1,29 @@
+package Servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/MyFirstServlet")
+public class MyFirstServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>My First Servlet program</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
